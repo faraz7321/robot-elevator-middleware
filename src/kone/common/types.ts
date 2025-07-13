b87bc13df8172f534d2ef0bd1d94c8e11a81c60f
Faraz Ahmad <faraz7321@gmail.com>
2025-07-13 22:50:50 +0200

    elevator call working

---
+++ src/kone/common/types.ts
@@ -0,0 +1,739 @@
+import WebSocket from 'ws'
+import { EventEmitter } from 'events'
+
+export const BUILDING_ID_PREFIX = 'building:'
+
+/**
+ * A string that represents a JWT access token
+ */
+export type AccessToken = string
+/**
+ * ISO timestamp string in format '2021-03-22T13:52:57.412Z'
+ */
+export type Timestamp = string
+/**
+ * Globally unique identifier for the building in the format `building:123456`
+ */
+export type BuildingId = string
+/**
+ * Globally unique identifier for the lift group in the format `group:123456:1`
+ */
+export type GroupId = string
+/**
+ * Globally unique identifier for the lift in the format `lift:123456:1:1`
+ */
+export type LiftId = string
+/**
+ * Globally unique identifier for the lift floor in the format `floor:123456:1:1:1`
+ */
+export type LiftFloorId = string
+/**
+ * Globally unique identifier for the side in the format `side:123456:1`
+ */
+export type SideId = string
+/**
+ * Globally unique identifier for the deck in the format `deck:123456:1:1:1`
+ */
+export type DeckId = string
+/**
+ * Globally unique identifier for the area in the format `area:123456:1000`
+ */
+export type AreaId = string
+/**
+ * Globally unique identifier for the door in the format `door:123456:1:1:1:1`
+ */
+export type DoorId = string
+/**
+ * Call type of a lift call
+ */
+export type CallType = 'normal' | 'robot'
+/**
+ * Call action of a lift call
+ */
+export type CallAction = 'destination'
+/**
+ * Status of the call. In a success case the status will progress from 'created' to 'approved' to 'served'. Once 'served' or 'cancelled' state is reached, there will be no further state updates.
+ */
+export type CallStatus = 'created' | 'approved' | 'served' | 'cancelled'
+/**
+ * Passenger guidance
+ */
+export type PassengerGuidance = 'wait_for_car' | 'enter_car' | 'stay_in_car' | 'exit_car' | 'call_cancelled'
+/**
+ * Cancel reason
+ */
+export type CallCancelReason =
+  | 'area_does_not_exist'
+  | 'area_is_locked'
+  | 'please_wait'
+  | 'cannot_serve_now'
+  | 'terminal_is_not_in_use'
+  | 'invalid_side'
+  | 'no_lifts_available'
+  | 'access_denied'
+  | 'unknown'
+
+/**
+ * A physical building.
+ */
+export type BuildingTopology = {
+  /**
+   * Identifier of the building.
+   */
+  buildingId: BuildingId
+  /**
+   * Lift groups in the building.
+   */
+  groups: Group[]
+  /**
+   * Areas in the building.
+   */
+  areas: Area[]
+}
+
+/**
+ * Logical lift group. Lift groups are the groups of lifts working in unison.
+ * For example the lift call is sent to certain lift group which then decides
+ * the lift that will serve the call.
+ */
+export type Group = {
+  /**
+   * Identifier of the group.
+   */
+  groupId: GroupId
+  /**
+   * Lifts in the group.
+   */
+  lifts: Lift[]
+}
+
+/**
+ * Lift is a physical lift in the building that belongs to certain lift group.
+ * Lifts can have multiple decks in them that serve lift users in different floors
+ * at the same time.
+ */
+export type Lift = {
+  /**
+   * Identifier of the lift.
+   */
+  liftId: LiftId
+  /**
+   * User visible name of the lift.
+   */
+  liftName: string
+  /**
+   * Decks of the lift.
+   */
+  decks: LiftDeck[]
+  /**
+   * Floors served by the lift.
+   */
+  floors: LiftFloor[]
+}
+
+/**
+ * Lift deck is one physical space inside the lift car.
+ */
+export type LiftDeck = {
+  /**
+   * Identifier of the deck.
+   */
+  deckId: DeckId
+  /**
+   * Identifier of the area inside the deck.
+   */
+  deckAreaId: AreaId
+  /**
+   * Areas served by the deck. This is a static structure describing all the areas served by the deck.
+   * At runtime depending on its actual vertical position the deck can serve zero, one or multiple areas.
+   */
+  areasServed: AreaId[]
+  /**
+   * Doors of the deck.
+   */
+  doors: Door[]
+}
+
+/**
+ * Floor served by the lift.
+ */
+export type LiftFloor = {
+  /**
+   * Identifier of the floor.
+   */
+  liftFloorId: LiftFloorId
+
+  /**
+   * Height of the floor in millimeters.
+   */
+  heightMillimeters: number
+
+  /**
+   * Level of the floor bottom in millimeters. Lowest floor has level of zero.
+   */
+  levelMillimeters: number
+
+  /**
+   * Areas served from the floor
+   */
+  areasServed: AreaId[]
+}
+
+/**
+ * Areas are the logical spaces in the building that the user can access.
+ * The areas are e.g. the source and destination of an lift call.
+ */
+export type Area = {
+  /**
+   * Identifier for the area.
+   */
+  areaId: AreaId
+  /**
+   * In which group side does this area reside. The side can be used to link to together areas and deck doors.
+   * Undefined if the area is not linked to any door.
+   */
+  sideId?: SideId
+  /**
+   * Short name for this area. This is the name usually visible in the lift control buttons.
+   */
+  shortName: string
+  /**
+   * Can this area be used to exit the building.
+   */
+  exitFloor: boolean
+  /**
+   * Height in millimeters.
+   */
+  heightMillimeters: number
+  /**
+   * Level in millimeters (starting from 0)
+   */
+  levelMillimeters: number
+}
+
+export type Door = {
+  /**
+   * Identifier for the door.
+   */
+  doorId: DoorId
+  /**
+   * Side of the door.
+   */
+  sideId: SideId
+}
+
+/**
+ * Types of requests available for the websocket APIs
+ */
+export type RequestType = 'lift-call' | 'create-session' | 'resume-session'
+
+/**
+ * Possible monitor events when making a lift call
+ */
+export type MonitorEvents = Array<'call' | 'deck' | 'door'>
+
+/**
+ * Unique request Identifier
+ */
+export type RequestId = string
+
+/**
+ * Unique session Identifier
+ */
+export type SessionId = string
+
+/**
+ * The time in seconds until the latest state data is re-sent
+ */
+export type ResendLatestStateUpToSeconds = number
+
+/**
+ *  amount of time it takes for the passenger to arrive to an elevator. Accepted range is from 1 to 600 seconds.
+ */
+export type PassengerArrivalTimeSeconds = number
+
+/**
+ *  Response type received by making a lift call, creating a session, resuming a session
+ */
+export type ResponseType = 'ok' | 'error'
+
+/**
+ *  Connection identifier
+ */
+export type ConnectionId = string
+
+/**
+ *  either close or keep connection open
+ */
+export type KeepAlive = boolean
+
+/**
+ * Destination call payload
+ */
+export type DestinationCallPayload = {
+  type: RequestType
+  callType: CallType
+  callAction: CallAction
+  requestId?: RequestId
+  buildingId: BuildingId
+  sourceId: AreaId
+  destinationId?: AreaId
+  monitorEvents?: MonitorEvents
+  keepAlive?: KeepAlive
+  passengerArrivalTimeSeconds?: PassengerArrivalTimeSeconds
+}
+
+/**
+ * Create session payload
+ */
+export type CreateSessionPayload = {
+  type: RequestType
+  requestId: RequestId
+}
+
+/**
+ * The connection status can be either open or closed
+ */
+export type ConnectionStatus = 'open' | 'closed'
+
+/**
+ * Resume session payload
+ */
+export type ResumeSessionPayload = {
+  type: RequestType
+  requestId: RequestId
+  sessionId: SessionId
+  resendLatestStateUpToSeconds?: ResendLatestStateUpToSeconds
+}
+
+export type StatusCode = 200 | 201 | 400 | 401 | 403 | 404 | 409 | 429 | 500
+
+/**
+ * Base interface for each message sent and received via WebSocket
+ */
+export interface WebSocketBase {
+  /**
+   * Each message must carry a type attribute that declares the contained request/event/response
+   */
+  type: string
+}
+
+/**
+ * Each request sent by the client will be responded with a response of either 'ok' or 'error' type
+ */
+export interface WebSocketResponse extends WebSocketBase {
+  type: ResponseType
+  /**
+   * Connection identifier that might be needed in case KONE customer service is contacted
+   */
+  connectionId: ConnectionId
+  /**
+   * Request ID either provided by the client or generated by the service
+   */
+  requestId: RequestId
+  /**
+   * HTTP analogous status code
+   */
+  statusCode: StatusCode
+  /**
+   * Human readable message used with error response
+   */
+  message?: string
+  /**
+   * Optional data sent by the service in response to the request
+   */
+  data?: object
+}
+
+/**
+ * Call state event
+ */
+export interface WebSocketCallState extends WebSocketBase {
+  type: 'lift-call-state'
+  timestamp: Timestamp
+  requestId: RequestId
+  callStatus: CallStatus
+  passengerGuidance?: PassengerGuidance
+  callType: CallType
+  callAction: CallAction
+  buildingId: BuildingId
+  groupId: GroupId
+  liftId?: LiftId
+  deckId?: DeckId
+  sourceDoorId?: DoorId
+  destinationDoorId?: DoorId
+  cancelReason?: CallCancelReason
+  sourceId: AreaId
+  destinationId?: AreaId
+}
+
+/**
+ * Session response with SessionData
+ */
+export interface WebSocketCreateSessionResponse extends WebSocketResponse {
+  type: ResponseType
+  connectionId: ConnectionId
+  requestId: RequestId
+  statusCode: StatusCode
+  data: SessionData
+}
+
+/**
+ * Response data to create-session request
+ */
+export interface SessionData {
+  sessionId: SessionId
+}
+
+/**
+ * Resume session response
+ */
+export interface WebSocketResumeSessionResponse extends WebSocketResponse {
+  type: ResponseType
+  connectionId: ConnectionId
+  requestId: RequestId
+  statusCode: StatusCode
+}
+
+/**
+ * WebSocketSession interface with emitted events
+ */
+export declare interface WebSocketSession {
+  // A previous session has been fully resumed
+  on(event: 'session-resumed', listener: (this: WebSocketSession) => void): this
+  // Event from service (any message excluding responses)
+  on(event: 'session-event', listener: (this: WebSocketSession, data: WebSocketBase) => void): this
+  // Resent event from WebSocket client
+  on(event: 'open', listener: (this: WebSocketSession) => void): this
+  // Resent event from WebSocket client
+  on(event: 'close', listener: (this: WebSocketSession, error: WebSocketResponse | Error) => void): this
+}
+
+/**
+ * Class definition for WebSocketSession
+ */
+export class WebSocketSession extends EventEmitter {
+  /**
+   * Connection status
+   */
+  connectionStatus: ConnectionStatus
+  /**
+   * Access token that can be updated, the new token will be used at next reconnect
+   */
+  accessToken: AccessToken
+  /**
+   * WebSocket client connection
+   */
+  connection: WebSocket
+  /**
+   * Session identifier
+   */
+  sessionId?: SessionId
+}
+
+/**
+ * Basic information of equipment
+ */
+export interface EquipmentInfo {
+  equipmentId: string
+  type: string
+  description?: string
+  serialNumber?: string
+  addressCity?: string
+  addressName?: string
+  addressStreet?: string
+  addressPostCode?: string
+  addressState?: string
+  addressCountry?: string
+}
+
+/**
+ * Maintenance status of equipment
+ */
+export interface EquipmentStatus {
+  entrapment?: boolean
+  status?: string
+  maintenance?: {
+    status?: string
+    technicianComment?: string
+  }
+}
+
+/**
+ * Service order of equipment
+ */
+export interface ServiceOrder {
+  serviceOrderId: string
+  activityType?: string
+  status?: string
+  description?: string
+  createdDateTime?: string
+  actualArrivalDateTime?: string
+  finishedDateTime?: string
+  invoices?: {
+    costAmount?: number
+    costCurrency?: string
+  }
+}
+
+/**
+ * Equipment Status API 2.0
+ * Availability information of equipment
+ * Refer to API portal documentation for more details of availability data format
+ */
+export interface Availability {
+  equipmentId: string
+  state: string
+  previousState?: string
+  activeAlertCount?: number
+  activeAlerts?: {
+    serviceNeedCode: string
+    serviceNeedVersion: string
+    activationTime: string
+    alertUUID: string
+  }[]
+  serviceMode?: string
+  devices?: {
+    deviceId: string
+    status: string
+    deviceType: string
+    connectionTime: string
+    disconnectReason?: string
+  }[]
+  lastUpdate?: string
+  equipmentInfo?: Omit<EquipmentInfo, 'equipmentId' | 'type'>
+}
+
+/**
+ * Equipment Status API 2.0
+ * Status information of equipment
+ * Refer to API portal documentation for more details of Status data format
+ */
+export interface Status {
+  equipmentId: string
+  entrapment: boolean
+  outOfOrder: boolean
+  status: string
+  equipmentStatus: string
+  equipmentOutOfOrderDate: string
+  equipmentBackInOrderDate: string
+  equipmentLocationId: string
+  equipmentLocationDescription: string
+  addressStreet: string
+  addressPostCode: string
+  addressCity: string
+  addressCountry: string
+  equipmentType: string
+  customerEquipmentLocationId: string
+  equipmentNumber: string
+  customerEquipmentNumber: string
+  equipmentFunctionalLocation: string
+  equipmentFunctionalLocationDescription: string
+  maintenance: {
+    status: string 
+  }
+  // equipmentInfo?: Omit<EquipmentInfo, 'equipmentId' | 'type'>
+  // lastUpdate?: string
+}
+
+/**
+ * Equipment Status API 2.0
+ * Movement information of equipment
+ * Refer to API portal documentation for more details of movement data format
+ */
+export interface Movement {
+  equipmentId: string
+  movementEventType: string
+  mode: string
+  decks: {
+    deckIndex: number
+    startFloor: {
+      floorIndex: number
+      marking: string
+    }
+    stopFloor: {
+      floorIndex: number
+      marking: string
+    }
+    loadPercentage: number
+    estimatedPersons: number
+  }[]
+  durationSeconds?: number
+  distanceMeters?: number
+  stopMode?: string
+  equipmentInfo?: Omit<EquipmentInfo, 'equipmentId' | 'type'>
+  lastUpdate?: string
+}
+
+/**
+ * Equipment Status API 2.0
+ * Elevator Door event information
+ * Refer to API portal documentation for more details of door event data format
+ */
+export interface DoorEvent {
+  equipmentId: string
+  lastUpdate: string
+  timestamp: string
+  doorOpenStartedTime: string
+  doorClosedTime: string
+  doorSideCode: number
+  doorSideDesc: string
+  deckIndexCode: number
+  deckIndexDesc: string
+  floor: number
+  floorMarking: string
+  loadBeforeDoorOpen: number
+  loadAfterDoorClose: number
+  modeCode: number
+  modeDesc: string
+  openingDuration: number
+  closingDuration: number
+  closingModeCode: number
+  closingModeDesc: string
+  reopensByPhotoCell: number
+  reopensByDOB: number
+  equipmentInfo: {
+    customerEquipmentNumber?: string
+    equipmentNumber?: string
+    serialNumber?: string
+    type?: string
+    description?: string
+    city?: string
+    countryCode?: string
+    postalCode?: string
+    name?: string
+    street?: string
+  }
+}
+
+/**
+ * Equipment Status API 2.0
+ * Elevator Button event information
+ * Refer to API portal documentation for more details of button event data format
+ */
+export interface Button {
+  equipmentId: string
+  lastUpdate: string
+  timestamp: string
+  landingCallType: {
+    floor: number
+    side: string
+    direction: string
+    type: string
+  }
+  equipmentInfo: {
+    customerEquipmentNumber?: string
+    equipmentNumber?: string
+    serialNumber?: string
+    type?: string
+    description?: string
+    city?: string
+    countryCode?: string
+    postalCode?: string
+    name?: string
+    street?: string
+  }
+}
+
+/**
+ * Equipment Status API 2.0
+ * Escalator movement information
+ * Refer to API portal documentation for more details of escalator movement data format
+ */
+export interface EscalatorMovement {
+  equipmentId: string
+  lastUpdate: string
+  statusChangeCode: string
+  statusChangeDesc: string
+  previousSpeed: number
+  currentSpeed: number
+  currentDirectionCode: number
+  currentDirectionDesc: string
+  previousDirectionCode: number
+  previousDirectionDesc: string
+  brakingDistance: number
+  serviceModeCode: number
+  serviceModeDesc: string
+  movementEventType: string
+  timestamp: string
+  equipmentInfo: {
+    equipmentNumber?: string
+    serialNumber?: string
+    type?: string
+    description?: string
+    city?: string
+    countryCode?: string
+    postalCode?: string
+    name?: string
+    street?: string
+  }
+}
+
+/**
+ * Service Info API 2
+ * List equipment service orders
+ * Refer to API portal documentation for more details of List equipment service orders data format
+ */
+export interface ListServiceOrder{
+  activityType: string
+  serviceOrderNumber: string
+  customerEquipmentNumber: string
+  workOrderNumber: string
+}
+
+/**
+ * Service Info API 2
+ * Get service order details
+ * Refer to API portal documentation for more details of Get service order details data format
+ */
+export interface GetServiceOrder{
+  activityType: string
+  status: string
+  description: string
+  actualArrivalDateTime: string
+  finishedDateTime: string
+  serviceOrderNumber: string
+  customerEquipmentNumber: string
+  workOrderNumber: string
+  jobDescription: string
+  additionalCommentsToCustomer: string
+  equipmentFailureReason: string
+  failedComponentCode: string
+  equipmentConditionOnArrival: string
+  equipmentConditionOnArrivalDesc: string
+  equipmentConditionOnDeparture: string
+  equipmentConditionOnDepartureDesc: string
+  technicianActionCode: string
+  entrapment: boolean
+  serviceContractNumber: string
+  earliestStartDate: string
+  workCenter: string
+  serviceTerritoryCode: string
+  serviceTerritoryName: string
+  workOrderStatus: string
+  serviceOrderType: string
+  serviceOrderTypeDesc: string
+  assemblyLocationCode: string
+  customerPurchaseOrderNumber: string
+  technicianRegistrationNumber: string
+  workTime: number
+  workTimeUnit: string
+  customer: string
+  workOrderPlannedStartDate: string
+  companyCode: string
+  contactPersonId: string
+  activityTypeCode: string
+  activityTypeCodeDesc: string
+  systemStatus: string
+  workOrderStatusCode: string
+  technicianComment: string
+  workOrderLineItems: {
+    operationText: string
+  }
+  descriptionOfWork: string
+  invoices:{
+    costAmount: number
+    costCurrency: string
+    name: string
+  }
+}
--- src/kone/elevator/service/elevator.service.ts
+++ src/kone/elevator/service/elevator.service.ts
@@ -1,34 +1,107 @@
 import { Injectable } from '@nestjs/common';
-import {LiftStatusRequestDTO} from "../dtos/status/LiftStatusRequestDTO";
-import {LiftStatusResponseDTO} from "../dtos/status/LiftStatusResponseDTO";
-import {CallElevatorRequestDTO} from "../dtos/call/CallElevatorRequestDTO";
-import {BaseResponseDTO} from "../../baseDtos/BaseResponseDTO";
-import {DelayDoorRequestDTO} from "../dtos/delay/DelayDoorRequestDTO";
-import {ReserveAndCancelRequestDTO} from "../dtos/reserve/ReserveAndCancelRequestDTO";
-import {ListElevatorsRequestDTO} from "../dtos/list/ListElevatorsRequestDTO";
-import {ListElevatorsResponseDTO} from "../dtos/list/ListElevatorsResponseDTO";
+import { LiftStatusRequestDTO } from '../dtos/status/LiftStatusRequestDTO';
+import { LiftStatusResponseDTO } from '../dtos/status/LiftStatusResponseDTO';
+import { CallElevatorRequestDTO } from '../dtos/call/CallElevatorRequestDTO';
+import { BaseResponseDTO } from '../../baseDtos/BaseResponseDTO';
+import { DelayDoorRequestDTO } from '../dtos/delay/DelayDoorRequestDTO';
+import { ReserveAndCancelRequestDTO } from '../dtos/reserve/ReserveAndCancelRequestDTO';
+import { ListElevatorsRequestDTO } from '../dtos/list/ListElevatorsRequestDTO';
+import { ListElevatorsResponseDTO } from '../dtos/list/ListElevatorsResponseDTO';
 
+import * as dotenv from 'dotenv';
+dotenv.config();
+import { v4 as uuidv4 } from 'uuid';
+import _ from 'lodash';
+
+import {
+  fetchAccessToken,
+  openWebSocketConnection,
+  validateClientIdAndClientSecret,
+} from '../../common/koneapi';
+
+/**
+ * Update these two variables with your own credentials or set them up as environment variables.
+ */
+const CLIENT_ID: string = process.env.CLIENT_ID || 'YOUR_CLIENT_ID'; // eg. 'dcf48ab0-a902-4b52-8c53-1a9aede716e5'
+const CLIENT_SECRET: string = process.env.CLIENT_SECRET || 'YOUR_CLIENT_SECRET'; // eg. '31d1329f8344fc12b1a960c8b8e0fc6a22ea7c35774c807a4fcabec4ffc8ae5b'
+const BUILDING_ID: string = process.env.BUILDING_ID || '4TFxWRCv23D';
+
+/**
+ * Function is used to log out incoming WebSocket messages
+ *
+ * @param {string} data data string from WebSocket
+ */
+const onWebSocketMessage = (data: string): void => {
+  let dataBlob = JSON.parse(data);
+
+  console.log('Incoming WebSocket message', dataBlob);
+  console.log('timing ' + new Date());
+};
+function getRequestId() {
+  return Math.floor(Math.random() * 1000000000);
+}
 @Injectable()
 export class ElevatorService {
+  listElevators(request: ListElevatorsRequestDTO): ListElevatorsResponseDTO {
+    return new ListElevatorsResponseDTO();
+  }
 
-    listElevators(request: ListElevatorsRequestDTO): ListElevatorsResponseDTO {
-        return new ListElevatorsResponseDTO();
-    }
+  getLiftStatus(request: LiftStatusRequestDTO): LiftStatusResponseDTO {
+    return new LiftStatusResponseDTO();
+  }
 
-    getLiftStatus(request: LiftStatusRequestDTO): LiftStatusResponseDTO {
-        return new LiftStatusResponseDTO();
-    }
+  async callElevator(
+    request: CallElevatorRequestDTO,
+  ): Promise<BaseResponseDTO> {
+    validateClientIdAndClientSecret(CLIENT_ID, CLIENT_SECRET);
+    let accessToken = await fetchAccessToken(CLIENT_ID, CLIENT_SECRET, [
+      'application/inventory',
+      `callgiving/group:${BUILDING_ID}:1`,
+    ]);
+    console.log('AccessToken successfully fetched');
 
-    callElevator(request: CallElevatorRequestDTO): BaseResponseDTO {
-        return new BaseResponseDTO();
-    }
+    // Select the first available building
+    const targetBuildingId = `building:${BUILDING_ID}`;
+    // Fetch the topology of the specific building
 
-    delayElevator(request: DelayDoorRequestDTO): BaseResponseDTO {
-        return new BaseResponseDTO();
-    }
+    // Open the WebSocket connection
+    const webSocketConnection = await openWebSocketConnection(accessToken);
+    console.log('WebSocket open ' + new Date());
 
-    reserveOrCancelCall(request: ReserveAndCancelRequestDTO): BaseResponseDTO {
-        return new BaseResponseDTO();
-    }
+    // Add handler for incoming messages
+    webSocketConnection.on('message', (data: any) => onWebSocketMessage(data));
 
+    // Build the call payload using the areas previously generated
+    const destinationCallPayload: any = {
+      type: 'lift-call-api-v2',
+      buildingId: targetBuildingId,
+      callType: 'action',
+      groupId: '1',
+      payload: {
+        request_id: getRequestId(),
+        area: 7000, //current floor
+        time: '2020-10-10T07:17:33.298515Z',
+        terminal: 1,
+        // terminal: 10011,
+        call: {
+          action: 3,
+          destination: 8000,
+        },
+      },
+    };
+
+    console.log(destinationCallPayload);
+    // execute the call within the open WebSocket connection
+    webSocketConnection.send(JSON.stringify(destinationCallPayload));
+
+    return new BaseResponseDTO();
+  }
+
+  delayElevator(request: DelayDoorRequestDTO): BaseResponseDTO {
+    return new BaseResponseDTO();
+  }
+
+  reserveOrCancelCall(request: ReserveAndCancelRequestDTO): BaseResponseDTO {
+    return new BaseResponseDTO();
+  }
 }
