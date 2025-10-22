00f6f1fedb035a229e43673c4137e08edbb063a2
Faraz Ahmad <faraz7321@gmail.com>
2025-10-22 13:54:19 +0200

    bug fix in ensureheartbeat

--- src/kone/elevator/dtos/call/CallElevatorResponseDTO.ts
+++ src/kone/elevator/dtos/call/CallElevatorResponseDTO.ts
@@ -1,3 +1,24 @@
 import { BaseResponseDTO } from '../../../baseDtos/BaseResponseDTO';
+import { IsNumber, IsOptional, IsString } from 'class-validator';
 
-export class CallElevatorResponseDTO extends BaseResponseDTO {}
+export class CallElevatorResponseDTO extends BaseResponseDTO {
+  @IsOptional()
+  @IsString()
+  sessionId?: string;
+
+  @IsOptional()
+  @IsNumber()
+  destination?: number;
+
+  @IsOptional()
+  @IsString()
+  connectionId?: string;
+
+  @IsOptional()
+  @IsNumber()
+  requestId?: number;
+
+  @IsOptional()
+  @IsNumber()
+  statusCode?: number;
+}
